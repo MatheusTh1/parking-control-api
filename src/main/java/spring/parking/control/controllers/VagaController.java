bae56a6297f5072e9d52af6bfe48e101e4d7c2fa
MatheusTh1 <matheushenriquesilva.th@gmail.com>
2022-07-18 14:24:01 -0300

    padronizado as URI dos end-points e collections do postman

--- src/main/java/spring/parking/control/controllers/VagaController.java
+++ src/main/java/spring/parking/control/controllers/VagaController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spring.parking.control.models.VagaModel;
 import spring.parking.control.repositories.VagaRepository;
@@ -17,27 +19,33 @@ import java.util.UUID;
 @NoArgsConstructor
 @SpringBootApplication
 @RestController
+@RequestMapping("/vagas")
 public class VagaController {
 
-    VagaRepository vagaEstacionamentoRepository;
+    VagaRepository vagaRepository;
 
-    @GetMapping("/vaga")
+    @GetMapping
     public List<VagaModel> buscarTodasVagas() {
-        return vagaEstacionamentoRepository.findAll();
+        return vagaRepository.findAll();
     }
 
-    @GetMapping("/vaga/{id}")
-    public VagaModel buscarVagaPorId(@PathVariable UUID id){
-        return vagaEstacionamentoRepository.findById(id).get();
-    }
-
-    @PostMapping("/vaga")
+    @PostMapping
     public VagaModel cadastarVeiculoNaVaga(@RequestBody VagaModel vagaModel) {
-        return vagaEstacionamentoRepository.save(vagaModel);
+        return vagaRepository.save(vagaModel);
     }
 
-    @DeleteMapping("/vaga/{id}")
+    @GetMapping("/{id}")
+    public VagaModel buscarVagaPorId(@PathVariable UUID id){
+        return vagaRepository.findById(id).get();
+    }
+    
+    @PutMapping("/{id}")
+    public VagaModel atualizarVeiculoNaVaga(@RequestBody VagaModel vagaModel){
+        return vagaRepository.save(vagaModel);
+    }
+
+    @DeleteMapping("/{id}")
     public void excluirVaga(@PathVariable UUID id){
-        vagaEstacionamentoRepository.deleteById(id);
+        vagaRepository.deleteById(id);
     }
 }
